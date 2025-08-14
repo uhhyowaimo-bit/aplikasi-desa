6e8c27ff2c1c6a85107f647b6e337a80ed0889a1
uhhyowaimo-bit <uhhyowaimo@gmail.com>
2025-08-14 17:10:58 +0800

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
