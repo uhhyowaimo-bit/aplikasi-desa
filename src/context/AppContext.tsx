0913d9b5b46999cb3619c25cd87cc2baa86b35b4
uhhyowaimo-bit <uhhyowaimo@gmail.com>
2025-08-15 14:26:54 +0800

    Update AppContext.tsx

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,31 +1,12 @@
-"use client";
+import { AppContextProvider } from "@/context/AppContext"; // Sesuaikan path sesuai kebutuhan
 
-import React, { createContext, useContext, useState, useEffect } from "react";
-
-// Membuat context
-const AppContext = createContext(null);
-
-// Provider untuk membungkus komponen
-export const AppContextProvider = ({ children }) => {
-  const [dark, setDark] = useState(false);
-
-  const toggleDark = () => setDark((prevState) => !prevState);
-
-  useEffect(() => {
-    const savedDarkMode = localStorage.getItem("darkMode");
-    if (savedDarkMode === "true") {
-      setDark(true);
-    }
-  }, []);
-
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <AppContext.Provider value={{ dark, toggleDark }}>
-      {children}
-    </AppContext.Provider>
+    <AppContextProvider>
+      <html lang="id">
+        <head />
+        <body>{children}</body>
+      </html>
+    </AppContextProvider>
   );
-};
-
-// Custom hook untuk mengakses context
-export const useAppContext = () => {
-  return useContext(AppContext);
-};
+}
