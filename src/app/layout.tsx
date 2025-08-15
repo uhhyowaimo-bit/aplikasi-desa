fc60ea04afb89e2cc22a59b7c65a005c23699801
uhhyowaimo-bit <uhhyowaimo@gmail.com>
2025-08-15 08:24:56 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,11 @@
-"use client";
-import { AppProvider } from "@/context/AppContext"; // Import AppProvider
-import ClientLayout from "@/components/ClientLayout"; // Import ClientLayout
+import { AppProvider } from "@/context/AppContext"; // Pastikan pathnya benar
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+function MyApp({ Component, pageProps }) {
   return (
-    <html lang="id">
-      <head>
-        <title>Aplikasi Desa</title> {/* Menambahkan title untuk aplikasi */}
-      </head>
-      <body>
-        <AppProvider> {/* Membungkus aplikasi dengan AppProvider */}
-          <ClientLayout>{children}</ClientLayout> {/* Konten halaman akan berada di sini */}
-        </AppProvider>
-      </body>
-    </html>
+    <AppProvider>
+      <Component {...pageProps} />
+    </AppProvider>
   );
 }
+
+export default MyApp;
