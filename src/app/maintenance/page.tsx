28d14414df90a9e9b0543f37915fb0323f1f4fde
uhhyowaimo-bit <uhhyowaimo@gmail.com>
2025-08-16 08:35:47 +0800

    Update page.tsx

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const MaintenancePage = () => {
   return (
     <div className="maintenance-page">
-      <h2 className="maintenance-header">We're Currently Under Maintenance</h2>
+      <h2 className="maintenance-header">We are Currently Under Maintenance</h2>
       <div className="maintenance-content">
         <p>Sorry for the inconvenience! Our website is undergoing scheduled maintenance.</p>
         <p>We are working hard to bring you the best experience. Please check back soon!</p>
